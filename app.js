56cbae4238f73d8575d16d777b270916810c7f6f
Andrew McOlash <amcolash@gmail.com>
2017-06-11 16:58:23 -0500

    Add in beginning files

---
+++ app.js
@@ -0,0 +1,81 @@
+window.onload = function() {
+  gapi.load('client', function() {
+    gapi.client.init({
+      'clientId': '246815826082-f2du3c4ince352fi8fbpn2hm0i6bf2fi.apps.googleusercontent.com',
+      'scope': 'profile https://www.googleapis.com/auth/fitness.activity.read',
+    });
+  });
+
+  gapi.load('auth2', function() {
+    gapi.signin2.render('my-signin', {
+      'scope': 'profile https://www.googleapis.com/auth/fitness.activity.read',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  });
+}
+
+function onSuccess(googleUser) {
+  updateUI();
+}
+
+function onFailure(error) {
+  console.log(error);
+  updateUI();
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    updateUI();
+  });
+}
+
+function getData() {
+  var start = new Date();
+  start.setHours(0,0,0,0);
+
+  var end = new Date(start.getTime());
+  end.setHours(23,59,59,9999);
+
+  var request = {
+    "aggregateBy": [{
+      "dataTypeName": "com.google.step_count.delta",
+      "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+    }],
+    "bucketByTime": { "durationMillis": 86400000 }, // one day
+    "startTimeMillis": start.getTime(),
+    "endTimeMillis": end.getTime()
+  };
+
+  var auth2 = gapi.auth2.getAuthInstance();
+  var signedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+  if (!signedIn) return;
+
+  gapi.client.request({
+    path: 'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
+    method: 'POST',
+    body: request
+  })
+  .then(function(res) {
+    // console.log(res);
+    if (res.result.bucket.length > 0) {
+      console.log(res.result.bucket[0].dataset[0].point[0].value[0].intVal);
+    }
+  }, function(err) {
+    console.error(err);
+  });
+}
+
+function updateUI() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  var signedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+
+  if (signedIn) {
+    $('#signed-in').show();
+    $('#signed-out').hide();
+  } else {
+    $('#signed-in').hide();
+    $('#signed-out').show();
+  }
+}
